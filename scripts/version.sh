070d1314f0e4d8669b3ad4b54d31109bdf448216
Christoph Gysin <christoph.gysin@gmail.com>
2011-03-24 23:06:52 +0100

    read cpack version from script

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-D=$(dirname ${0})
-sed -ne 's/#define ADDP_VERSION.*"\(.*\)"/\1/p' ${D}/../src/addp/version.h
+D="$(dirname ${0})/.."
+VERSION_H="src/addp/version.h"
+
+VERSION=$(sed -ne 's/#define ADDP_VERSION.*"\(.*\)"/\1/p' ${D}/${VERSION_H})
+
+case "${1}" in
+    major) echo ${VERSION%%.*};;
+    minor) echo ${VERSION} | cut -d. -f2;;
+    patch) echo ${VERSION##*.};;
+    full)  echo ${VERSION};;
+esac
